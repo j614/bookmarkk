355baf5b35d862d7a03db23c14ece741e62c023d
j614 <jashik244@gmail.com>
2019-11-01 17:15:55 +0300

    1.11.19

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -37,9 +37,13 @@ def register(request):
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data(['password']))
+            new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'account/register_done.html', {'new_user': new_user})
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form', user_form})
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
