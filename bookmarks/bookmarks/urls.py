4f3892c58b5fdda2c47ae65d454a530d23ad022b
j614 <jashik244@gmail.com>
2019-11-04 14:52:33 +0300

    1.11.19

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 
 """Функция static() подходит для разработки, но не для использования впроизводстве. Никогда не обслуживайте ваши статические файлы
