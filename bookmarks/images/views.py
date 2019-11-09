7c5864ad93491f94c4f5a62c62b9c653509e4b23
j614 <jashik244@gmail.com>
2019-11-09 05:49:19 +0300

    28.10.19

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
 
 @login_required
@@ -28,7 +29,7 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
 
-
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -42,6 +43,7 @@ def image_like(request):
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
+
         except:
             pass
     return JsonResponse({'status': 'ko'})
