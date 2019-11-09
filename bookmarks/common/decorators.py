e1f31af6896787ad2a44dc3262bad3ceee24122d
j614 <jashik244@gmail.com>
2019-11-09 06:16:07 +0300

    28.10.19

--- bookmarks/common/decorators.py
+++ bookmarks/common/decorators.py
@@ -5,6 +5,6 @@ def ajax_required(f):
     def wrap(request, *args, **kwargs):
         if not request.is_ajax():
             return HttpResponseBadRequest()
-    wrap.__doc__=f.__doc__
-    wrap.__name__=f.__name
+    wrap.__doc__= f.__doc__
+    wrap.__name__= f.__name__
     return wrap
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,8 @@ from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -47,3 +49,22 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'ko'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator('images', 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html',
+                  {'section': 'images', 'images': images})
