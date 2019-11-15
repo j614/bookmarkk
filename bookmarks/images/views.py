e871a589d316f937fd18cfbd2e1103d1d69a48a3
j614 <jashik244@gmail.com>
2019-11-15 06:09:02 +0300

    добавлены: -отображение потока автивности в views images и account

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import ImageCreateForm
-from django.shortcuts import get_object_or_404
-from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import ImageCreateForm
+from .models import Image
+from common.decorators import ajax_required
+from actions.utils import create_action
+from django.conf import settings
 
 
 @login_required
@@ -18,8 +19,10 @@ def image_create(request):
         if form.is_valid():
             cd = form.cleaned_data
             new_item = form.save(commit=False)
+
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
             return redirect(new_item.get_absolute_url())
     else:
@@ -30,6 +33,7 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
 
 @ajax_required
 @login_required
@@ -42,10 +46,10 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
-
         except:
             pass
     return JsonResponse({'status': 'ko'})
